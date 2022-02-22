ccc2712ffc3c3b6ca78a06cf1ed232f563939e5c
Karthikeyan Nadesan <Karthik@Bharathis-Air.attlocal.net>
2022-02-22 11:05:38 -0500

    Modified Calculator file

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,9 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+ * Application for calculator.
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
